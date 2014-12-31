98bfbedafdd6c433f674aa56d6ae0e60ea9140ba
ruxchuk <ruxchuk@gmail.com>
2014-12-31 11:50:40 +0700

    job, company view

---
+++ WP Theme/libs/pages/job-view.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 31/12/2557
+ * Time: 11:09 น.
+ */
+
+get_template_part("header");
+get_template_part("libs/nav");
+?>
+
+    <section class="container-fluid" style="margin-top: 10px;">
+
+        <div class="container wrapper">
+            <div class="row">
+                <div class="col-md-8">
+
+                    <div class="clearfix" style="border: 1px #ddd solid; border-radius: 5px; background: #fff; padding: 10px; margin-bottom: 10px;">
+
+                        <img src="<?php echo get_template_directory_uri(); ?>/libs/img/job-desc-01.jpg" style="width: 100%"/>
+
+                        <h4 class="font-color-3 clearfix" style="">
+                            <span class="pull-left">Secretary (japanese speaking)</span>
+                            <span class="pull-right"><i class="glyphicon glyphicon-star font-color-BF2026"></i></span>
+                        </h4>
+                        <h5 class="font-color-BF2026 clearfix" style="">BJC - Berli jucker Public Company Limited.
+                            <a href="company-profile.php" target="_blank">(View company profile)</a></h5>
+                        <hr/>
+                        <h5><strong>Company Profile</strong></h5>
+                        <p>
+                            When the roots are strong, nothing can stop the growth.<br/><br/>
+
+                            Founded in 1882, BJC has today a nationwide presence; a part of everyday life for every Thai who uses and relies on our products and services.
+                            BJC is a leading trading and manufacturing company with a staff is over 5,000 and annual sales of 20billions baht, we enjoy a reputation for superior services and products. And as a highly dynamic business group with several subsidiaries we currently invite high-caliber and professional candidates to join this vibrantly active enterprise.<br/><br/>
+
+                            For our supply chain: The BJC Group's five core business areas are:<br/>
+                            1.Industrial Supply Chain<br/>
+                            2.Consumer Supply Chain<br/>
+                            3.Healthcare Supply Chain<br/><br/>
+
+                            Build your career with one of the best in the business.
+                            BJC is searching for high potential employees in many areas such as accounting, marketing, medical, chemical, engineering, printing technology etc.
+                        </p>
+                        <div class="jumbotron clearfix">
+                            <h5><strong>Job Detail</strong></h5>
+                            <table style="width: 100%">
+                                <tr>
+                                    <td style="50%">Date Posted :</td>
+                                    <td style="50%">29 September, 2014</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">Category :</td>
+                                    <td style="50%">Secretary</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">Job Type :</td>
+                                    <td style="50%">Permanent </td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">JLPT LEVEL :</td>
+                                    <td style="50%">N2</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">JOB LOCATION :</td>
+                                    <td style="50%">Bangkok</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">JAPANESE SKILLS :</td>
+                                    <td style="50%">Good</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">Salary :</td>
+                                    <td style="50%">Negotiable</td>
+                                </tr>
+                                <tr>
+                                    <td style="50%">Working Day :</td>
+                                    <td style="50%">Mon-Fri 8.00 – 17.00</td>
+                                </tr>
+                            </table>
+                        </div>
+
+                        <h5><strong>Job Description</strong></h5>
+                        <p>
+                            Handle all secretarial and administrative work, responsible for incoming and outgoing documents and reports, check the correctness of documents for signature, take note and prepare minutes of meeting, prepare presentation      on Power Point and also arrange meetings and appointments.<br/>
+
+                        <ol>
+                            <li>Liaising with Oversea staff in Japan by telephone, and email</li>
+                            <li>Responsible for all administration and secretarial jobs</li>
+                            <li>Manage and be proactive for schedule</li>
+                            <li>Prepare meeting agenda,  take minutes of meeting, and arrange appointment</li>
+                            <li>Coordinate with all department and outside people</li>
+                            <li>Maintains customer confidence and protects operations by keeping information confidential.</li>
+                            <li>Prepares reports by collecting information</li>
+                        </ol>
+                        **Job Location : BJC Sukhumvit  42, Bangkok**
+                        </p>
+                        <h5 class="margin-top-20"><strong>Qualification</strong></h5>
+                        <p>
+                        <ol>
+                            <li>Thai nationality</li>
+                            <li>Bachelor's Degree in any field</li>
+                            <liGood communication and interpersonal skills</ol>
+                        <li>Strong knowledge of written and spoken English and Japanese at least JLPT 2</li>
+                        <li>Ability to use personal computer (MS office / Outlook)</li>
+                        <li>Hard-working and high service mind.</li>
+                        <li>Other tasks following Manager’s assignment</li>
+                        </ol>
+                        </p>
+                        <h5 class="margin-top-20"><strong>Contact</strong></h5>
+                        <p>
+                            Berli Jucker Public Co.,Ltd.<br/>
+                            Human Resource Division<br/>
+                            99 Soi Rubia Sukhumvit 42, Phrakanong, Klongtoey, Bangkok 10110<br/>
+                            Tel. 02-367-1410<br/>
+                            Fax. 02-367-1594<br/>
+                            E-mail. pattarat@bjc.co.th, kusumad@bjc.co.th
+                        </p>
+
+                        <div class="col-md-12 margin-top-20">
+                            <button type="button" id="applyNow" name="applyNow" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-ok"></span>
+                                apply now
+                            </button>
+                            <button type="button" id="addFavorite" name="addFavorite" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-star"></span>
+                                add favorite
+                            </button>
+                            <button type="button" id="viewAllFavorite" name="viewAllFavorite" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-folder-open"></span>
+                                all favorite
+                            </button>
+                            <button type="button" id="map" name="map" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-map-marker"></span>
+                                map
+                            </button>
+                            <button type="button" id="print" name="print" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-print"></span>
+                                print
+                            </button>
+                            <button type="button" id="share" name="share" class="btn btn-default no-border col-md-2">
+                                <span class="glyphicon glyphicon-share"></span>
+                                share
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <img src="<?php echo get_template_directory_uri(); ?>/libs/img/blank-banner-ads-01.png" style="width: 100%; height: auto;"/>
+
+                </div>
+
+                <?php get_template_part("libs/pages/sidebar"); ?>
+            </div>
+        </div>
+
+    </section>
+
+<?php get_template_part("footer"); ?>
--- WP Theme/single.php
+++ WP Theme/single.php
@@ -1,54 +1,63 @@
-<?php get_header(); ?>
+<?php
+$postType = get_post_type(get_the_ID());
+if ($postType == 'job')
+    get_template_part('libs/pages/job-view', get_post_format());
+else if ($postType == 'company')
+    get_template_part('libs/pages/company-view', get_post_format());
+else {
 
-			<div id="content">
-<h1>Is single</h1>
-				<div id="inner-content" class="wrap cf">
+    get_template_part("header", get_post_format());
+    get_template_part("libs/nav", get_post_format());
+    ?>
+    <div id="content">
+        <h1>Is single</h1>
 
-					<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+        <div id="inner-content" class="wrap cf">
 
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
-							<?php
-								/*
-								 * Ah, post formats. Nature's greatest mystery (aside from the sloth).
-								 *
-								 * So this function will bring in the needed template file depending on what the post
-								 * format is. The different post formats are located in the post-formats folder.
-								 *
-								 *
-								 * REMEMBER TO ALWAYS HAVE A DEFAULT ONE NAMED "format.php" FOR POSTS THAT AREN'T
-								 * A SPECIFIC POST FORMAT.
-								 *
-								 * If you want to remove post formats, just delete the post-formats folder and
-								 * replace the function below with the contents of the "format.php" file.
-								*/
-								get_template_part( 'post-formats/format', get_post_format() );
-							?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-						<?php endwhile; ?>
+                    <?php
+                    /*
+                     * Ah, post formats. Nature's greatest mystery (aside from the sloth).
+                     *
+                     * So this function will bring in the needed template file depending on what the post
+                     * format is. The different post formats are located in the post-formats folder.
+                     *
+                     *
+                     * REMEMBER TO ALWAYS HAVE A DEFAULT ONE NAMED "format.php" FOR POSTS THAT AREN'T
+                     * A SPECIFIC POST FORMAT.
+                     *
+                     * If you want to remove post formats, just delete the post-formats folder and
+                     * replace the function below with the contents of the "format.php" file.
+                    */
+                    get_template_part('post-formats/format', get_post_format());
 
-						<?php else : ?>
+                    ?>
 
-							<article id="post-not-found" class="hentry cf">
-									<header class="article-header">
-										<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-									</header>
-									<section class="entry-content">
-										<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-									</section>
-									<footer class="article-footer">
-											<p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
-									</footer>
-							</article>
+                <?php endwhile; ?>
 
-						<?php endif; ?>
+                <?php else : ?>
 
-					</div>
+                    <article id="post-not-found" class="hentry cf">
+                        <header class="article-header">
+                            <h1><?php _e('Oops, Post Not Found!', 'bonestheme'); ?></h1>
+                        </header>
+                        <section class="entry-content">
+                            <p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'bonestheme'); ?></p>
+                        </section>
+                        <footer class="article-footer">
+                            <p><?php _e('This is the error message in the single.php template.', 'bonestheme'); ?></p>
+                        </footer>
+                    </article>
 
-					<?php get_sidebar(); ?>
+                <?php endif; ?>
 
-				</div>
-
-			</div>
-
-<?php get_footer(); ?>
+            </div>
+            <?php get_template_part("pages/sidebar", get_post_format()); ?>
+        </div>
+    </div>
+    <?php get_template_part("footer", get_post_format());
+}
+?>
